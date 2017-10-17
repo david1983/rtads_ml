ff4ede163533b03432146078da6af3646748e2fe
D4v1d3 <y37arf68>
2017-10-17 11:43:25 +0100

    test

--- tests/test.py
+++ tests/test.py
@@ -1,33 +1,5 @@
-# content of test_sample.py
 
-# Import libraries
-import matplotlib
-matplotlib.use('Agg')
-import base64
-import json
-import pandas               as pd
-from io                 import StringIO,BytesIO
-from matplotlib         import pyplot as plt
-from pandas.plotting import scatter_matrix
-
-def plot(plot, name):     
-    image = BytesIO()    
-    fig = plot.get_figure()                   
-    fig.savefig(name, format='png')
-    # image.seek(0)  # rewind to beginning of file
-    # figdata_png = image.getvalue()     
-    # d = base64.encodestring(image.getvalue()).decode('ascii')   
-    # return '%s' % (d)
-
-def analyse():
-    data = pd.read_csv("./test.csv")    
-    data.plot()
-    # plt.show()            
-    sm = scatter_matrix(data, figsize=(24,24))    
-    n = 0
-    print(plot(sm[0][0], name="./" + str(n) + ".png"))        
-            
-
-analyse()
+def sum(a,b):
+    return a+b
 def test_answer():    
-    assert 0==0
+    assert sum(1,1) == 2
